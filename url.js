7c5f70067e174b00fd7b778b2888c9a129f4ae54
shiuan1223 <144261055+shiuan1223@users.noreply.github.com>
2023-09-07 08:24:08 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/QWsfuJVwC1GD5Txn9',
+	'https://forms.gle/XkZP6kCb9sKgJAAs7'
 ];
